ffd10a4d5f87db929b60948a03153819ba9f4327
NgThanhDanh <danhnguyenmw@gmail.com>
2025-05-18 20:44:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'mc.example.com', // 🧠 ← Thay bằng IP hoặc tên server Minecraft bạn muốn treo
-  port: 25565,             // Cổng (nếu không biết thì để 25565)
-  username: 'AFK_Bot_123', // 🧠 ← Tên acc Minecraft (acc crack nếu server cho phép)
+  host: 'NTDSMP1165.aternos.me', // 🧠 ← Thay bằng IP hoặc tên server Minecraft bạn muốn treo
+  port: 51261,             // Cổng (nếu không biết thì để 25565)
+  username: 'BoMayCanTat', // 🧠 ← Tên acc Minecraft (acc crack nếu server cho phép)
   version: false           // Tự động chọn phiên bản phù hợp
 });
 
